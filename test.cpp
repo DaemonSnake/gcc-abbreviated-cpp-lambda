5906c4c96d945155ea2f89747c0a06f1eced0856
Bastien Penavayre <bastienPenava@gmail.com>
2017-09-14 07:31:58 +0200

    update the example for testing

--- test.cpp
+++ test.cpp
@@ -15,19 +15,39 @@ constexpr bool is_rvalue_ref(T&&)
     return std::is_rvalue_reference_v<T&&>;
 }
 
+int&& f(int&& x) noexcept(true) { return >>x; }
+
 int main()
 {
     //syntax
-    constexpr auto square = []<class T>(T&& x) => x * x;
+    constexpr auto square = [](auto&& x) => x * x;
+    square(5);
     static_assert(square(5) == 25);
 
-    //decltype over return expr
     invoke([]<class T>(T&& x)
            => test(std::forward<T>(x)));
 
-    //forward operator
+    //unary operator >> : perfect forwarding
     constexpr auto check = [](auto&&x) => is_rvalue_ref(>>x);
-    static_assert(check(42));
     int i = 0;
+
+    check(42);
+    check(i);
+    static_assert(check(42));
     static_assert(!check(i));
+
+#define TEST_TYPE_EXCEPT(name, call, return, expr)              \
+    call;                                                       \
+    static_assert(std::is_same_v<decltype(call), return>);      \
+    static_assert(noexcept(call) == noexcept(expr))
+
+    auto c2 = [](auto&& x) => x;
+    auto c3 = [](auto&& x) noexcept(true) => f(>>x); //noexcept(noexcept(ret_expr)) todo
+    auto c4 = [](auto& x) => x;
+    auto c5 = [](auto x) => x;
+
+    TEST_TYPE_EXCEPT(c2, c2(5), int, 5);
+    TEST_TYPE_EXCEPT(c3, c3(5), int&&, f(5));
+    TEST_TYPE_EXCEPT(c4, c4(i), int&, i);
+    TEST_TYPE_EXCEPT(c5, c5(5), int, 5);
 }
