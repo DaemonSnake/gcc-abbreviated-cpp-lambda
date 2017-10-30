8c861c4b0f9073ed332d69dc5c0adadc9c31de26
Bastien Penavayre <bastienPenava@gmail.com>
2017-10-30 21:44:42 +0100

    Update test file to conform to the proposal

--- test.cpp
+++ test.cpp
@@ -1,21 +1,20 @@
 #include <functional>
 #include <type_traits>
 
-void check_return_type_are_correct();
 void check_forward();
 void check_raii();
 void check_noexcept_spec();
 
 struct Widget {};
-bool test(int) => true;
-bool test(Widget) => false;
+auto test(int) => true;
+auto test(Widget) => false;
 void invoke(std::function<bool(int)>) {}
 void invoke(std::function<bool(std::string)>) {}
 
 template <class T>
-constexpr bool is_rvalue_ref(T&&) => std::is_rvalue_reference_v<T&&>;
+constexpr auto is_rvalue_ref(T&&) => std::is_rvalue_reference_v<T&&>;
 
-int&& f(int&& x) noexcept(true) => (>>x);
+auto f(int&& x) => (>>x);
 
 int main()
 {
@@ -26,7 +25,6 @@ int main()
 
     check_raii();
     check_forward();
-    check_return_type_are_correct();
     check_noexcept_spec();
 }
 
@@ -54,51 +52,6 @@ void check_optional_type_for_parameter()
     static_assert(sqrt(42) == 42 * 42);
     [](x) { static_assert(std::is_rvalue_reference_v<decltype(x)>); }(42);
     [](x...) { static_assert((is_rvalue_ref(>>x) && ...)); }(42);
-}
-
-template<class T> constexpr auto func_1(T&& x) => (>>x);
-template<class T> constexpr auto func_2(T&& x) -> decltype((>>x)) => (>>x);
-template<class T> constexpr auto func_3(T&& x) => (x);
-template<class T> constexpr auto func_4(T&& x) -> decltype((x)) => (x);
-
-void check_return_type_are_correct()
-{
-    int i = 0, &&v = 0;
-
-    constexpr auto make_is_same = []<class L, class R>(L&&, R&&) {
-        static_assert(std::is_same_v<L, R>);
-    };
-    constexpr auto lambda_1 = []<class T>(T&& x) => (>>x);
-    constexpr auto lambda_2 = []<class T>(T&& x) -> decltype((>>x)) => (>>x);
-    constexpr auto lambda_3 = []<class T>(T&& x) => (x);
-    constexpr auto lambda_4 = []<class T>(T&& x) -> decltype((x)) => (x);
-
-#define SAME_F(funcA, funcB, args...)           \
-    make_is_same(funcA(args), funcB(args))
-
-    SAME_F(lambda_1, lambda_2, 42);
-    SAME_F(lambda_1, lambda_2, i);
-    SAME_F(lambda_1, lambda_2, v);
-    SAME_F(lambda_1, lambda_2, >>i);
-    SAME_F(lambda_1, lambda_2, >>v);
-
-    SAME_F(lambda_3, lambda_4, 42);
-    SAME_F(lambda_3, lambda_4, i);
-    SAME_F(lambda_3, lambda_4, v);
-    SAME_F(lambda_3, lambda_4, >>i);
-    SAME_F(lambda_3, lambda_4, >>v);
-
-    SAME_F(func_1, func_2, 42);
-    SAME_F(func_1, func_2, i);
-    SAME_F(func_1, func_2, v);
-    SAME_F(func_1, func_2, >>i);
-    SAME_F(func_1, func_2, >>v);
-
-    SAME_F(func_3, func_4, 42);
-    SAME_F(func_3, func_4, i);
-    SAME_F(func_3, func_4, v);
-    SAME_F(func_3, func_4, >>i);
-    SAME_F(func_3, func_4, >>v);
 }
 
 void check_noexcept_spec()
